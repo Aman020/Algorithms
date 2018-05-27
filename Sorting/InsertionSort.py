bb611163a3b710be2561da771647e082fa900ebb
a_m_a_n <amanbhayana@yahoo.in>
2018-05-27 20:45:08 +0530

    Insertion sort - completed

--- Sorting/InsertionSort.py
+++ Sorting/InsertionSort.py
@@ -1,2 +1,18 @@
 #Insertion Sort o(n2)
 # Sort a given array by using Insertion SOrt Algorithm
+
+
+def insertionSort(array):
+    for j in range(1, len(array)):
+        key = array[j]
+        i = j-1
+        while i >= 0 and key < array[i]:
+
+            array[i+1] = array[i]
+            i = i-1
+        array[i +1 ] = key
+
+arr = [12,10,2,100,23,1243,3,24,56,765,3,23,]
+insertionSort(arr)
+for i in range(len(arr)):
+    print (arr[i])
